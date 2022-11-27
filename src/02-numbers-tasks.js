a19b3f46aea8bf90f92d28514775e64745d3cfb1
unknown <tebba@yandex.ru>
2022-11-27 23:41:15 +0100

    feat:added solutions for number-tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -208,12 +208,13 @@ function isPrime(n) {
   if (n <= 1 || n % 2 === 0 || n % 3 === 0) {
     return false;
   }
+
   for (let i = 5; i * i <= n; i += 6) {
     if (n % i === 0 || n % (i + 2) === 0) {
       return false;
     }
-    return true;
   }
+  return true;
 }
 
 /**
