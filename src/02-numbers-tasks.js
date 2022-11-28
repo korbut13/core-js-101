3c4985d18c6f02eb2a559eeccf6442465a318e14
unknown <tebba@yandex.ru>
2022-11-28 23:39:32 +0100

    feat:added new solutions for arrays-tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -232,8 +232,13 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const newValue = Number(value);
+  if (typeof value === 'number' || newValue) {
+    return +value;
+  }
+
+  return def;
 }
 
 module.exports = {
